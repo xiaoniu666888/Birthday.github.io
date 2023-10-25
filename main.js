e5b74160c11563ac741aec006736ee1bfe2a6a68
xiaoniu666888 <107358468+xiaoniu666888@users.noreply.github.com>
2023-10-25 23:46:44 +0800

    Update main.js
    update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var S = {
         if (i !== -1) {
             S.UI.simulate(decodeURI(action).substring(i + 3));
         } else {
-            S.UI.simulate('|#countdown 3||祝|牛|闯|生|日|快|乐||Happy|birthday|to|you|一起|不断|进步|鸭|#rectangle|');
+            S.UI.simulate('|#countdown 3||祝|刘|雅|静|生|日|快|乐||Happy|birthday|to|linya|#rectangle|');
         }
 
         S.Drawing.loop(function() {
